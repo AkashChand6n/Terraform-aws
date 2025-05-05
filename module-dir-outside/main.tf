3bbf639e0e649a70f462130e346c093ca2f8e5ac
Akashchand6n <akashanoop123456@gmail.com>
2025-05-05 06:02:52 +0000

    added terraform module outside the dir

---
+++ module-dir-outside/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2" {
+  source        = "../modules/modules/ec2_instance"
+  ami           = var.ami
+  instance_type = var.instance_type
+}
+
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
+}
